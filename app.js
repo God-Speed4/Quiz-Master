65e58ee30f130e8ad83a438846279a373c5b899b
Cortez <90485463+God-Speed4@users.noreply.github.com>
2022-04-04 11:36:47 -0500

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3003;
-app.get('/', (req, res) => res.send('Hello World!!'));
-app.listen(port, () => console.log('Server is running on port '+ port));

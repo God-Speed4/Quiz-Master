a5590ad611e43afa6f3ac8fe37eac9ddbaeacb39
Cortez <90485463+God-Speed4@users.noreply.github.com>
2022-04-06 13:46:00 -0500

    Delete server directory

--- server/routes/quiz.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import {getQuiz, createQuiz} from '../controllers/quiz.js'
-const router = express.Router();
-
-router.get('/', getQuiz);
-router.post('/', createQuiz);
-export default router; 
